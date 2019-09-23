8aaac6d96eb0857a565789d6810b608e02e2b691
Pradeepa Senanayake <pradeepa.senanayake@resmed.com.au>
2019-09-23 22:43:24 +0100

    Half way through fugitive installation

--- setup_vim_env.sh
+++ setup_vim_env.sh
@@ -9,17 +9,16 @@ echo "Creating the .vimrc"
 REPO_DIR=$(pwd)
 HOME_DIR=~
 VIM_DIR=$HOME_DIR/.vim
-VIM_RC=.vimrc
+VIM_RC=$HOME_DIR/.vimrc
 
 BUNDLE_DIR=$VIM_DIR/bundle
 PLUGIN_DIR=$VIM_DIR/plugin
 
-cd $HOME_DIR
 mv $VIM_RC $VIM_RC.backup
 touch $VIM_RC
-mkdir $VIM_DIR
-mkdir $BUNDLE_DIR
-mkdir $PLUGIN_DIR
+mkdir -p -v $VIM_DIR
+mkdir -p -v $BUNDLE_DIR
+mkdir -p -v $PLUGIN_DIR
 
 cat <<EOT >> $VIM_RC
 set relativenumber
@@ -30,12 +29,16 @@ EOT
 
 echo "Setting up CTRLP"
 
-cd $VIM_DIR
-mkdir bundle
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
-cd $HOME_DIR
+cd $BUNDLE_DIR
+git clone https://github.com/kien/ctrlp.vim.git ctrlp.vim
 echo "set runtimepath^=~/.vim/bundle/ctrlp.vim" >> $VIM_RC
+cd $REPO_DIR
 
 echo "Setting up cscope"
 cp $REPO_DIR/vim_configs/cscope_maps.vim $PLUGIN_DIR
 
+echo "Setting up fugitive"
+cd $BUNDLE_DIR
+git clone https://github.com/tpope/vim-fugitive.git vim-fugitive.vim
+echo "set runtimepath^=~/.vim/bundle/vim-fugitive.vim" >> $VIM_RC
+cd $REPO_DIR
