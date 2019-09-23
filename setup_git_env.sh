d079e43a6ca3ede00061732bbd28ab3593448ae4
Pradeepa Senanayake <pradeepa.senanayake@resmed.com.au>
2019-09-23 12:11:51 +1000

    Fixed an issue in git env

--- setup_git_env.sh
+++ setup_git_env.sh
@@ -6,5 +6,5 @@ USER_NAME="Pradeepa Senanayake"
 USER_EMAIL="pradeepa.senanayake@resmed.com.au"
 #USER_EMAIL="pradeepa.kck@gmail.com"
 
-git config --global user.name $USER_NAME
+git config --global user.name "$USER_NAME"
 git config --global user.email $USER_EMAIL
