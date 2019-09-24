859920468026750ba900e0283ff1b94e747b516a
Pradeepa Senanayake <pradeepa.senanayake@resmed.com.au>
2019-09-24 21:19:43 +1000

    Added more vim configurations

--- setup_vim_env.sh
+++ setup_vim_env.sh
@@ -24,7 +24,35 @@ cat <<EOT >> $VIM_RC
 set relativenumber
 set syntax=on
 set cindent
+
+set foldmethod=indent
+set foldlevel=99
+
+"split navigations
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+"search for text
 nmap <F3> :silent grep! -R --include=*.c --include=*.h <C-R><C-W> *<CR> :redr!<CR> :copen<CR>
+
+nmap <space> za
+
+"python PEP8 indentation
+au BufNewFile,BufRead *.py
+	\ set tabstop=4 |
+	\ set softtabstop=4 |
+	\ set shiftwidth=4 |
+	\ set textwidth=79 |
+	\ set expandtab |
+	\ set autoindent |
+	\ set fileformat=unix
+
+"Flag whitespaces
+highlight BadWhitespace ctermbg=darkgreen guibg=lightgreen
+au BufRead,BufNewFile *.py,*.pyw,*.c,*.h match BadWhitespace /\s\+$/
+
 EOT
 
 echo "Setting up CTRLP"
@@ -42,3 +70,10 @@ cd $BUNDLE_DIR
 git clone https://github.com/tpope/vim-fugitive.git vim-fugitive.vim
 echo "set runtimepath^=~/.vim/bundle/vim-fugitive.vim" >> $VIM_RC
 cd $REPO_DIR
+
+echo "Setting up SimplyFold"
+cd $BUNDLE_DIR
+git clone https://github.com/tmhedberg/SimpylFold.git SimplyFold.vim
+echo "set runtimepath^=~/.vim/bundle/SimpleFold.vim" >> $VIM_RC
+cd $REPO_DIR
+
