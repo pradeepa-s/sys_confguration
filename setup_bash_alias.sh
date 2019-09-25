4205abdfd1487d839a48087a6097e3bfd8dd9c75
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2019-09-25 23:45:48 +1000

    Add personal alias

--- setup_bash_alias.sh
+++ setup_bash_alias.sh
@@ -9,4 +9,7 @@ cp .bashrc .bashrc.pradeepa.backup
 cat <<EOT >> .bashrc
 # Pradeepa's aliases goes here:
 alias tmux='tmux -2'
+alias myp='cd /cygdrive/i/my_projects/'
+alias mypi='cd /cygdrive/i/my_projects/itsi'
+
 EOT
